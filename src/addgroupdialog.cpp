a8afb875ca14e6f178d2de342c6fba4032795f21
KT <anya.a.ellis@gmail.com>
2025-02-17 00:28:09 -0500

    addgroupdialog.ui pre autoButton elements setup

--- src/addgroupdialog.cpp
+++ src/addgroupdialog.cpp
@@ -23,20 +23,56 @@ void AddGroupDialog::init(QList<QString> headers)
 
 void AddGroupDialog::additionalUiSetup()
 {
-
+    // File Path Actions
     ui->filePathEdit->setReadOnly(true);
-
     connect(ui->toolButton, &QToolButton::clicked, this, &AddGroupDialog::onToolButtonClicked);
+
+    // Header Related Actions
+    ui->headersCheckBox->setChecked(false);
+    ui->vRadioButton->setDisabled(true);
+    ui->hRadioButton->setDisabled(true);
+
+    connect(ui->headersCheckBox, &QCheckBox::stateChanged, this, &AddGroupDialog::shouldDisableHeaderStyle);
+
+    // Field Separator
+    QStringList seps = {",", ";", "/", tr("tab")};
+    ui->fieldSepComboBox->addItems(seps);
+    ui->fieldSepComboBox->setEditable(true);
+
+    // Automate Button
+    ui->autoButton->setDisabled(true);
 
 }
 
 
 void AddGroupDialog::onToolButtonClicked()
 {
-    QString filepath = QFileDialog::getSaveFileName(this, tr("Choose File"),
+    QString filepath = QFileDialog::getOpenFileName(this, tr("Choose File"),
                                                     ui->filePathEdit->text(),
                                                     tr("Spreadsheet (*.csv *.tsv)"));
 
 
-    ui->filePathEdit->setText(filepath);
+    if(filepath != ""){
+        ui->filePathEdit->setText(filepath);
+
+        //
+        ui->autoButton->setDisabled(false);
+
+        if(filepath.last(4) == ".tsv")
+            ui->fieldSepComboBox->setCurrentIndex(3);
+    }
+
+}
+
+void AddGroupDialog::shouldDisableHeaderStyle()
+{
+    if(ui->headersCheckBox->checkState() == Qt::Checked){
+        ui->hRadioButton->setDisabled(false);
+        ui->vRadioButton->setDisabled(false);
+    }
+    else
+    {
+        ui->hRadioButton->setDisabled(true);
+        ui->vRadioButton->setDisabled(true);
+    }
 }
--- src/headers/addgroupdialog.h
+++ src/headers/addgroupdialog.h
@@ -19,6 +19,7 @@ public:
 
 private slots:
     void onToolButtonClicked();
+    void shouldDisableHeaderStyle();
 
 
 private:
